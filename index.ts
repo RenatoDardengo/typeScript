952cb057730c30b5eed8dda1fca66d88f3042053
Renato Dardengo <rdardengo01@gmail.com>
2022-11-21 22:30:34 -0300

    Tipos de em TS

---
+++ index.ts
@@ -0,0 +1,50 @@
+// Introdução
+/*var number1:number=40
+var number2:number=60
+
+
+function sum (num1:number, num2:number){
+     console.log (` Os números informados são: ${num1} e ${num2}. A resultado de sua soma é: ${num1+num2}`)
+}*/
+
+// sum (number1, number2)
+
+
+/************************************************************************************** */
+//TIPOS NUMÉRICOS
+
+/*let x:number = 70.236
+console.log (x)
+console.log(typeof x)
+console.log(x.toPrecision(3))*/
+
+
+/*************************************************************************** */
+//TIPOS STRING
+/*let firstName:string= "Renato";
+let lastName:string = "Dardengo";
+console.log(firstName.toUpperCase())
+console.log (`O nome completo é ${firstName+" "+lastName}`)*/
+
+/************************************************************************** */
+//TIPO BOOLEAN
+
+/*let x:boolean=true;
+console.log (x);
+
+x=false;
+console.log (x)
+    //type inference
+let w=true
+console.log(typeof w)*/
+
+/***************************************************************** */
+//criando arquivo de configuração => tsc --init
+
+/*************** */
+
+//CONVERSÃO DE TIPOS
+/*let num:number=50;
+let x=num.toString();
+console.log (`O valor convertido da variável foi ${x}`)*/
+
